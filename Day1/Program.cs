7a58c7ca054744ed60719839f33aa4f9cb1e77a1
Steven Palmer <steve@stevewpalmer.uk>
2022-12-01 10:27:01 +0000

    Add project files and day 1 solution

---
+++ Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿int elfCalories = 0;
+List<int> calories = new();
+
+foreach (string line in File.ReadAllLines("day1.txt")) {
+    if (string.IsNullOrEmpty(line)) {
+        calories.Add(elfCalories);
+        elfCalories = 0;
+    } else {
+        elfCalories += Convert.ToInt32(line);
+    }
+}
+calories.Add(elfCalories);
+
+Console.WriteLine($"Puzzle 1 answer : Total calories = {calories.MaxBy(e => e)}");
+Console.WriteLine($"Puzzle 2 answer : Sum of top 3 calories = {calories.OrderByDescending(e => e).Take(3).Sum()}");
