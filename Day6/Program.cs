70ff0aa19524a71121a2ec3d7062f369aa2576bc
Steven Palmer <steve@stevewpalmer.uk>
2022-12-06 17:08:50 +0000

    Solution for day 6.

---
+++ Day6/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = File.ReadAllText("day6.txt");
+
+int FindMarker(int packetSize) {
+
+    for (int index = packetSize; index < input.Length; index++) {
+        string packet = input.Substring(index - packetSize, packetSize);
+        if (new HashSet<char>(packet).Count == packetSize) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+Console.WriteLine($"Puzzle 1 answer : Answer = {FindMarker(4)}");
+Console.WriteLine($"Puzzle 2 answer : Answer = {FindMarker(14)}");
