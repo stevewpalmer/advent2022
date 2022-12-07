7cd6f84b4cd7eb6ece8078f08fd75eff6e3ea6e3
Steven Palmer <steve@stevewpalmer.uk>
2022-12-07 19:45:50 +0000

    Solution for day 7.

---
+++ Day7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+Stack<int> directorySizes = new();
+Stack<int> traverse = new();
+
+foreach (string line in File.ReadAllLines("day7.txt")) {
+
+    string[] parts = line.Split(' ');
+    if (parts[0] == "$" && parts[1] == "cd") {
+        if (parts[2] == "..") {
+            int fileSize1 = traverse.Pop();
+            directorySizes.Push(fileSize1);
+            traverse.Push(traverse.Pop() + fileSize1);
+        } else {
+            traverse.Push(0);
+        }
+    } else {
+        if (int.TryParse(parts[0], out int fileSize2)) {
+            traverse.Push(traverse.Pop() + fileSize2);
+        }
+    }
+}
+
+int totalDiskSize = 0;
+while (traverse.Count > 0) {
+    totalDiskSize += traverse.Pop();
+    directorySizes.Push(totalDiskSize);
+}
+
+Console.WriteLine($"Puzzle 1 answer : Answer = {directorySizes.Where(d => d <= 100000).Sum()}");
+Console.WriteLine($"Puzzle 2 answer : Answer = {directorySizes.Where(d => d >= totalDiskSize - 40000000).Min()}");
