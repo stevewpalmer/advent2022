5998b0f6070e8306541d16118bb538075f9dbf94
Steven Palmer <steve@stevewpalmer.uk>
2022-12-12 17:37:51 +0000

    Solutions for days 8 and 9.

---
+++ Day8/Program.cs
@@ -0,0 +1,44 @@
+﻿int[][] input = File.ReadAllLines("day8.txt").Select(l => l.ToCharArray().Select(p => p - '0').ToArray()).ToArray();
+int maxColumn = input[0].Length;
+int maxRow = input.Length;
+
+bool Left(int x, int y) => x == 0 || !input[y][..x].Any(p => p >= input[y][x]);
+bool Right(int x, int y) => x == maxColumn - 1 || !input[y][(x + 1)..maxColumn].Any(p => p >= input[y][x]);
+bool Above(int x, int y) => y == 0 || !input[..y].Select(p => p[x]).Any(q => q >= input[y][x]);
+bool Below(int x, int y) => y == maxRow - 1 || !input[(y + 1)..maxRow].Select(p => p[x]).Any(q => q >= input[y][x]);
+
+int ScenicCount(IEnumerable<int> array, int m) {
+    int count = 0;
+    foreach (int x in array) {
+        ++count;
+        if (x >= m) {
+            break;
+        }
+    }
+    return count;
+}
+
+int LeftScenic(int x, int y) => ScenicCount(input[y][..x].Reverse(), input[y][x]);
+int RightScenic(int x, int y) => ScenicCount(input[y][(x + 1)..maxColumn], input[y][x]);
+int AboveScenic(int x, int y) => ScenicCount(input[..y].Reverse().Select(p => p[x]), input[y][x]);
+int BelowScenic(int x, int y) => ScenicCount(input[(y + 1)..maxRow].Select(p => p[x]), input[y][x]);
+
+int countOfVisible = 0;
+int maxScenicScore = 0;
+
+for (int row = 0; row < maxRow; row++) {
+    for (int column = 0; column < maxColumn; column++) {
+
+        if (Left(column, row) || Right(column, row) || Above(column, row) || Below(column, row)) {
+            ++countOfVisible;
+        }
+
+        maxScenicScore = Math.Max(LeftScenic(column, row) *
+                                  RightScenic(column, row) *
+                                  AboveScenic(column, row) *
+                                  BelowScenic(column, row), maxScenicScore);
+    }
+}
+
+Console.WriteLine($"Puzzle 1 answer : Answer = {countOfVisible}");
+Console.WriteLine($"Puzzle 2 answer : Answer = {maxScenicScore}");
---
+++ Day9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+
+string[] input = File.ReadAllLines("day9.txt");
+
+int ModelKnots(int count) {
+
+    HashSet<Point> map = new();
+    Point[] knots = new Point[count];
+    foreach (string line in input) {
+        string[] code = line.Split();
+
+        int xDirection = 0;
+        int yDirection = 0;
+        switch (code[0]) {
+            case "R": xDirection = 1; break;
+            case "L": xDirection = -1; break;
+            case "U": yDirection = -1; break;
+            case "D": yDirection = 1; break;
+        }
+
+        int steps = Convert.ToInt32(code[1]);
+        while (steps-- > 0) {
+
+            knots[0].Offset(xDirection, yDirection);
+
+            int knotIndex = 0;
+            while (++knotIndex < count) {
+
+                int dx = knots[knotIndex - 1].X - knots[knotIndex].X;
+                int dy = knots[knotIndex - 1].Y - knots[knotIndex].Y;
+
+                if (Math.Abs(dx) > 1 || Math.Abs(dy) > 1) {
+                    knots[knotIndex].X += Math.Sign(dx);
+                    knots[knotIndex].Y += Math.Sign(dy);
+                }
+            }
+            map.Add(knots[count - 1]);
+        }
+    }
+    return map.Count;
+}
+
+Console.WriteLine($"Puzzle 1 answer : Answer = {ModelKnots(2)}");
+Console.WriteLine($"Puzzle 2 answer : Answer = {ModelKnots(10)}");
